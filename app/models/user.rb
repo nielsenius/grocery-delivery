f2faf5e88e90451af32b3c27b747f7387a687289
Matthew Nielsen <mqnielsen@me.com>
2015-03-03 21:33:29 -0500

    Added test data using rake db:populate command; installed Foundation; set up some basic views.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
 
-  validates_presence_of :password, on: :create 
-  validates_presence_of :password_confirmation, on: :create 
-  validates_confirmation_of :password, message: "does not match"
-  validates_length_of :password, minimum: 6, message: "must be at least 6 characters long", allow_blank: true
+  # validates_presence_of :password, on: :create 
+  # validates_presence_of :password_confirmation, on: :create 
+  # validates_confirmation_of :password, message: "does not match"
+  # validates_length_of :password, minimum: 6, message: "must be at least 6 characters long", allow_blank: true
  
   validates_presence_of :phone_number
   validates_format_of :phone_number, with: /\A(\d{10}|\(?\d{3}\)?[-. ]\d{3}[-.]\d{4})\z/, message: "should be 10 digits (area code needed) and delimited with dashes only"
--- app/views/items/index.json.jbuilder
+++ app/views/items/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@items) do |item|
-  json.extract! item, :id, :name, :amount
+  json.extract! item, :id, :name, :amount, :grocery_list_id
   json.url item_url(item, format: :json)
 end
