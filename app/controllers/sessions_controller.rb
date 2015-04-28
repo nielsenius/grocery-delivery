67254190181a19865519108c09e40ca71ca2cbf1
Matthew Nielsen <mqnielsen@me.com>
2015-04-27 21:26:55 -0400

    Changed logout redirection.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to home_path, notice: "Logged out!"
+    redirect_to login_path, notice: "Logged out!"
   end
   
 end
